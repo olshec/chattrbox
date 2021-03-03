844c1adcf1730af00bb2062b607fd8b703fcd6db
Oleg Shestakov <olshec@gmail.com>
2021-03-03 18:33:29 +0300

    added errorHandler

---
+++ extract.js
@@ -0,0 +1,16 @@
+let path = require('path');
+
+let extractFilePath = function(url) {
+    let filePath;
+    let fileName = 'index.html';
+    
+    if(url.length > 1) {
+        fileName = url.substring(1);
+    }
+    console.log('The fileName is: ' + fileName);
+    
+    filePath = path.resolve(__dirname, 'app', fileName);
+    return filePath;
+};
+
+module.exports = extractFilePath;
--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 let http = require("http");
+let fs = require('fs');
+let extract = require('./extract');
 
-let server = http.createServer(function(req,res){
+let handlerError = function(err, res) {
+    res.writeHead(404);
+    res.end();
+};
+
+let server = http.createServer(function(req, res){
     console.log('Responding to a request.');
-    res.end('<h1>Hello, World!</h1>');
+    
+    let filePath = extract(req.url);
+    fs.readFile(filePath, function(err, data) {
+        if(err) {
+            handlerError(err, res);
+            return;
+        } else {
+            res.end(data);
+        }
+    });
 });
 server.listen(3000);
