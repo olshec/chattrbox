7f7ff93678823677d085c60b306cc4a8093db58f
Oleg Shestakov <olshec@gmail.com>
2021-03-03 21:45:56 +0300

    created chat

--- websockets-server.js
+++ websockets-server.js
@@ -1,17 +1,25 @@
 let WebSocket = require('ws');
 let WebSocketServer = WebSocket.Server;
 let port = 3001;
+
 let ws = new WebSocketServer({
     port: port
 });
 
 console.log('websockets server started');
 
+let messages = [];
 ws.on('connection', function(socket) {
     console.log('client connection established');
     
+    messages.forEach(function(msg) {
+        socket.send(msg);
+    })
     socket.on('message', function(data){
         console.log('message received: ' + data);
-        socket.send(data);
+        messages.push(data);
+        ws.clients.forEach(function(clientSocket){
+            clientSocket.send(data);
+        });
     });
 });
